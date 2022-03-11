34a4c2963be88405edcf8839cf5648dee67446b3
Martin <48153829+sumos77@users.noreply.github.com>
2022-03-11 19:45:15 +0100

    Added more text

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 print("Hello World")
 number = 1 + 2
 print(number)
-print("Hello Smith")
+print("Hello Smith and everyone else")
 repeat_times = int(input("How many times would you like this repeated: "))
 times_repeated = 13
 while times_repeated <= repeat_times:
